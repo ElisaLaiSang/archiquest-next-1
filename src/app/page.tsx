f07c330a9df0b2b6afce1fd190cf79c4d65836b9
ElisaLaiSang <166709406+ElisaLaiSang@users.noreply.github.com>
2024-04-24 19:31:05 +1000

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/timelapse">Timelapse Demo </Link>
-        <Link href="/artcritic">Art Critic Demo </Link>
-        <Link href="/map">Map Demo </Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-400">
+      <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-between font-mono text-sm lg:flex">
+        <Link href="/map" className="mb-3">Play Under the Weather </Link>
+        <input id="playerName" type="text" className="p-2 rounded-lg mb-3" placeholder="Name"></input>
+        <input id="playerSpecialTalent" type="text" className="p-2 rounded-lg mb-3" placeholder="Special Talent"></input>
       </div>
     </main>
   );
 }
+
