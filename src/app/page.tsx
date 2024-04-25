32ecacddbb4bba70a59193f005e6382d7798203c
ElisaLaiSang <166709406+ElisaLaiSang@users.noreply.github.com>
2024-04-25 12:55:34 +1000

    startPageBackgorund

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   const [playerSpecialSkill, setPlayerSpecialSkill] = useState(""); // Initialize playerSpecialSkill state
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-cover bg-center" style={{ backgroundImage: `url('/startPageImage.png')` }}>
       <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/undertheweather" className="mb-3">Play Under the Weather </Link>
+        <Link href="/undertheweather" className="mb-3 text-white">Play Under the Weather </Link>
         <input 
           id="playerName" 
           type="text" 
@@ -19,15 +19,7 @@ export default function Home() {
           value={playerName}
           onChange={(e) => setPlayerName(e.target.value)} // Update playerName state onChange
         ></input>
-        <input 
-          id="playerSpecialSkill" 
-          type="text" 
-          className="p-2 rounded-lg mb-3" 
-          placeholder="Special Skill"
-          value={playerSpecialSkill}
-          onChange={(e) => setPlayerSpecialSkill(e.target.value)} // Update playerSpecialSkill state onChange
-        ></input>
-        <p className="z-10 max-w-5xl">Instructions: Your goal is to create the most convincing excuse that will get you a day off work. Rearrange the set of events given and see whether you will be successful.</p>
+        <p className="p-20 text-white">Instructions: Craft an excuse to take the day off work. Select and drag the given prompts into a order you believe will be convincing enough to get a day off work.</p>
       </div>
     </main>
   );
--- src/app/undertheweather/page.tsx
+++ src/app/undertheweather/page.tsx
@@ -5,7 +5,10 @@ import { describeImagePrompt } from "@/ai/prompts";
 import TagCloud from "@/components/TagCloud";
 import ImageGallery from "@/components/ImageGallery";
 import { getGroqCompletion } from "@/ai/groq";
-import { generateImageFal, generateVoice } from "@/ai/fal";
+import { generateImageFal} from "@/ai/fal";
+
+    // Get the player's name and special skill
+    const playerName = document.getElementById("playerName");
 
 type Artwork = {
   description: string;
@@ -20,19 +23,15 @@ export default function UnderTheWeatherPage() {
   const [keywords, setKeywords] = useState<string>("Selected Keywords...");
   const [artworks, setArtworks] = useState<Artwork[]>([]);
   const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
-  const [message, setMessage] = useState<string>("Generate Excuse");
+  const [message, setMessage] = useState<string>("Generate Assessment");
   const [score, setScore] = useState<string>("0");
 
   async function handleCreate() {
-    setMessage("Generating Excuse...");
+    setMessage("Generating Assessment...");
 
-    // Get the player's name and special skill
-    const playerName = document.getElementById("playerName") as HTMLInputElement | null;
-    const playerSpecialSkill = document.getElementById("playerSpecialSkill") as HTMLInputElement | null;
-  
     //generate the image description
     const description = await getGroqCompletion(
-      `Describe the scenario that ${playerName} has told you about why they need the day off work using the following: ${keywords}.Take into consideration their ${playerSpecialSkill}.`,
+      `Describe the scenario that ${playerName} has told you about why they need the day off work using the following: ${keywords}.`,
       200,
       describeImagePrompt
     );
@@ -45,7 +44,7 @@ export default function UnderTheWeatherPage() {
     const critique = await getGroqCompletion(
       `The employee has given the following description: ${description}`,
       100,
-      "As the employer, assess their reason. If it is plausible, say that it is a valid reason to take the day off work. If it is not plausible do not use the word valid."
+      "As the employer, assess their reason. If the reason is plausible, give them a day of leave."
     );
 
      // Determine if the critique is considered plausible
@@ -68,13 +67,13 @@ export default function UnderTheWeatherPage() {
     };
     setArtworks([...artworks, newArtwork]);
     setSelectedArtwork(newArtwork);
-    setMessage("Generate Excuse");
+    setMessage("Generate Assessment");
 
 
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-amber-100 border border-neutral-800 custom-border rounded-2xl">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="flex flex-col">
           <TagCloud
